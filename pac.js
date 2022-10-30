9992f4a5d995ce4faf4fe0bff12f936e62262593
threadburner64 <82733494+threadburner64@users.noreply.github.com>
2022-10-30 19:14:25 -0400

    Update pac.js

--- pac.js
+++ pac.js
@@ -3,19 +3,19 @@ function URLPath(href) {
     var match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)([\/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/);
     return match && {
 
-        pathname: match[5],
+        pathname: match[5]
 
-    }
-}
+    };
+};
 
 
 function FindProxyForURL(url, host) { 
-
+  
     if (host == "www.roblox.com") {
-       var UrlPath = URLPath(url)
-
-
-       if (UrlPath.pathname == "/my/avatar") {return "PROXY 127.0.0.7:1337" }
+       var UrlPath = URLPath(url);
+       if (UrlPath.pathname == "/my/avatar") {
+            return "PROXY 127.0.0.7:1337";
+        };
     };
    return "DIRECT";
 }
