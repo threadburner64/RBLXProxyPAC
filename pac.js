8d0981d55c229f4fc7ae50704829e09c7ff13c97
threadburner64 <82733494+threadburner64@users.noreply.github.com>
2022-10-30 17:41:01 -0400

    Update pac.js

--- pac.js
+++ pac.js
@@ -1,5 +1,5 @@
 function FindProxyForURL(url, host) { 
-    if (host == "roblox.com") {
+    if (host == "www.roblox.com") {
         return "PROXY 4.5.6.7:8080; PROXY 7.8.9.10:8080"; 
     };
    return "DIRECT";
