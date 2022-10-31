5f5fdee2da956b430fba2f2686e37752e631f9dc
threadburner64 <82733494+threadburner64@users.noreply.github.com>
2022-10-30 20:23:45 -0400

    Update pac.js

--- pac.js
+++ pac.js
@@ -1,20 +1,22 @@
-function split_url(_url) {
-  var url = _url.split( '//' );
-  if (url[0] === "http:" || url[0] === "https:") {
-    var protocol = url[0] + "//";
-    var host = url[1].split( '/' )[0];
-    url = protocol + host;
-    var path = _url.split(url)[1];
-    return {
-      protocol: protocol,
-      host: host,
-      path: path
-    };
-  };
-};
+
 
 function FindProxyForURL(url, host) { 
 
+      function split_url(_url) {
+        var url = _url.split( '//' );
+        if (url[0] === "http:" || url[0] === "https:") {
+          var protocol = url[0] + "//";
+          var host = url[1].split( '/' )[0];
+          url = protocol + host;
+          var path = _url.split(url)[1];
+          return {
+            protocol: protocol,
+            host: host,
+            path: path
+          };
+        };
+    };
+  
     if (host == "www.roblox.com") {
    
        var match = split_url(url);
